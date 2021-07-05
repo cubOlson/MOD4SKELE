0142d0153bd9359600c8014fa799ddfb522488f2
cubOlson <cub.olson@gmail.com>
2021-07-05 16:16:59 -0400

    added fetch

--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -1,4 +1,7 @@
-window.addEventListener('DOMContentLoaded', (event) => {
+window.addEventListener('DOMContentLoaded', async(event) => {
     console.log('DOM fully loaded and parsed');
-    if (req.session.auth) console.log(user)
+
+    const weather = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Chattanooga&appid=f379b3857c328f3085b067f960c64d13').then(response => response.json());
+
+    console.log('WEATHER', weather)
 });
